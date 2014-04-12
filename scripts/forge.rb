2153db136b373448c485565a531002bec8f0c6e5
Taylor Otwell <taylorotwell@gmail.com>
2014-04-12 00:25:30 -0500

    Update forge.rb

--- scripts/forge.rb
+++ scripts/forge.rb
@@ -4,9 +4,9 @@ class Forge
 		config.vm.box = "chef/ubuntu-13.10"
 
 		config.vm.provider "virtualbox" do |vb|
-			vb.customize ["modifyvm", :id, "--memory", "2048"]
-			vb.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
-			vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
+		  vb.customize ["modifyvm", :id, "--memory", "2048"]
+		  vb.customize ["modifyvm", :id, "--natdnsproxy1", "on"]
+		  vb.customize ["modifyvm", :id, "--natdnshostresolver1", "on"]
 		end
 
 		# Configure Port Forwarding
